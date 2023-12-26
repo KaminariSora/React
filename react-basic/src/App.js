1ccd3665146321474e80a93baae7c60baa0e7e60
Your NameKaminariSora <nonthacha.h@gmail.com>
2023-12-26 17:13:47 +0700

    First React Hook

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -6,11 +6,6 @@ import { useState } from "react";
 
 function App() {
   const design = { color: "#fff", textAlign: "start", fontSize: "1.5rem" }
-  const initData = [
-    { id: 1, title: "ค่ารักษาพยาบาล", amount: "2000" },
-    { id: 2, title: "ค่าเช่า", amount: "20000" },
-    { id: 3, title: "ค่าเดินทาง", amount: "1000" },
-  ]
   const [items,setItems] = useState([])
   const OnAddNewItem = (newItem) => {
     setItems((prevItem) => {
