36e72824e1917796a28bb83da0f3228c79015763
Your NameKaminariSora <nonthacha.h@gmail.com>
2023-12-26 17:00:17 +0700

    Learning react

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,14 +1,27 @@
 import Transaction from "./Components/Transaction";
+import FormComponent from "./Components/FormComponent";
 import "./Components/App.css"
-
-const Title =() => <h1>โปรแกรมบัญชี รายรับ-รายจ่าย</h1>
+import "./Components/FormComponent.css"
+import { useState } from "react";
 
 function App() {
-  const design = {color:"#fff", textAlign:"center", fontSize:"1.5rem"}
+  const design = { color: "#fff", textAlign: "start", fontSize: "1.5rem" }
+  const initData = [
+    { id: 1, title: "ค่ารักษาพยาบาล", amount: "2000" },
+    { id: 2, title: "ค่าเช่า", amount: "20000" },
+    { id: 3, title: "ค่าเดินทาง", amount: "1000" },
+  ]
+  const [items,setItems] = useState([])
+  const OnAddNewItem = (newItem) => {
+    setItems((prevItem) => {
+      return [newItem,...prevItem]
+    })
+  }
   return (
-    <section className="contianer" style={design}>
-      <Title/>
-      <Transaction/>
+    <section style={design}>
+      <FormComponent OnAddItem={OnAddNewItem} />
+      <Transaction items={items} />
+
     </section>
   );
 }
--- react-basic/src/Components/Transaction.js
+++ react-basic/src/Components/Transaction.js
@@ -1,20 +1,11 @@
 import Item from "./Item"
 import "./Transaction.css"
 
-const Transaction =()=> {
-    const data = [
-        {id:1, title:"ค่ารักษาพยาบาล", amount:"2000"},
-        {id:2, title:"ค่าเช่า", amount:"20000"},
-        {id:3, title:"ค่าเดินทาง", amount:"1000"},
-        {id:4, title:"ค่าเดินทาง", amount:"1000"},
-        {id:5, title:"ค่าเดินทาง", amount:"1000"},
-        {id:6, title:"ค่าเดินทาง", amount:"1000"},
-        {id:7, title:"ค่าเดินทาง", amount:"1000"},
-        {id:8, title:"ค่าเดินทาง", amount:"1000"},
-    ]
+const Transaction =(props)=> {
+    const {items} = props
     return (
         <ul className="item-list">
-            {data.map((element) => {
+            {items.map((element) => {
                 return (
                     <Item title={element.title} amount={element.amount}/>
                 )
